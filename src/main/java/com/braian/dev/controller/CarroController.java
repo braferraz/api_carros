cd461f18093733ffa3718e5fe2cc1ad0946a414b
= <=>
2020-12-17 18:13:06 -0200

    Metodo POST

--- src/main/java/com/braian/dev/controller/CarroController.java
+++ src/main/java/com/braian/dev/controller/CarroController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +36,12 @@ public class CarroController {
 		else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		
 	}
+	
+	@RequestMapping(value = "/carro", method = RequestMethod.POST)
+	public Carro Post(@Validated @RequestBody Carro carro) {
+		return _carroRepository.save(carro);
+	}
+	
+	
 }
